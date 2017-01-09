e878d9e0a71e1d6ce7b2794213704f2232bf14e2
Nama Karlina <widyastutikarlina@gmail.com>
2017-01-10 06:57:58 +0700

    RecyclerView List Round Image

---
+++ app/src/main/java/model/Hotel.java
@@ -0,0 +1,20 @@
+package model;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Iitutsa on 10/01/2017.
+ */
+public class Hotel {
+    public String judul;
+    public String deskripsi;
+    public Drawable foto;
+
+    public Hotel(String judul, String deskripsi, Drawable foto)
+    {
+        this.judul = judul;
+        this.deskripsi = deskripsi;
+        this.foto = foto;
+    }
+}
+
